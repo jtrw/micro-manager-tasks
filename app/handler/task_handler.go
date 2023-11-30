bb1752ec1fbc6681b56eb8b0b90a0096e075aed2
Nil Borodulia <nil.b@oxtech.org>
2023-11-30 13:15:44 +0300

    refactoring service

---
+++ app/handler/task_handler.go
@@ -0,0 +1,131 @@
+package handler
+
+import (
+    "context"
+    "encoding/json"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/bson/primitive"
+	"net/http"
+	"log"
+)
+
+type Handler struct {
+	Client *mongo.Client
+}
+
+func NewHandler(client *mongo.Client) Handler {
+	return Handler{Client: client}
+}
+
+// Task структура для зберігання завдань
+type Task struct {
+	UUID     string      `json:"uuid" bson:"uuid"`
+	Count    int         `json:"count" bson:"count"`
+	Type     string      `json:"type" bson:"type"`
+	Status   string      `json:"status" bson:"status"`
+	Subtasks []SubTask   `json:"subtasks" bson:"subtasks"`
+}
+
+// SubTask структура для зберігання підзавдань
+type SubTask struct {
+	UUID   string `json:"uuid" bson:"uuid"`
+	Type   string `json:"type" bson:"type"`
+	Status string `json:"status" bson:"status"`
+}
+
+
+// createTask обробник запиту POST /createTask
+func (h Handler) CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var task Task
+	_ = json.NewDecoder(r.Body).Decode(&task)
+
+	task.Status = "created"
+	task.UUID = primitive.NewObjectID().Hex()
+	task.Subtasks = []SubTask{}
+
+	collection := h.Client.Database("micro-tasks").Collection("tasks")
+	_, err := collection.InsertOne(context.Background(), task)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Failed to create task")
+		return
+	}
+
+	response := map[string]string{"uuid": task.UUID}
+	json.NewEncoder(w).Encode(response)
+}
+
+// addSubTask обробник запиту POST /addSubTask
+func (h Handler) AddSubTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var subTask SubTask
+	_ = json.NewDecoder(r.Body).Decode(&subTask)
+
+	uuid := r.URL.Query().Get("uuid")
+    log.Println(uuid)
+	collection := h.Client.Database("micro-tasks").Collection("tasks")
+	filter := bson.M{"uuid": uuid}
+	update := bson.M{
+		"$push": bson.M{"subtasks": subTask},
+	}
+
+	res, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+	    log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Failed to add subtask")
+		return
+	}
+	log.Println(res)
+
+	// Перевіряємо кількість підзавдань та змінюємо статус, якщо потрібно
+	var task Task
+	err = collection.FindOne(context.Background(), filter).Decode(&task)
+	if err != nil {
+	    log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Failed to update task status!")
+		return
+	}
+
+	if len(task.Subtasks) == task.Count {
+		update := bson.M{
+			"$set": bson.M{"status": "done"},
+		}
+
+		_, err := collection.UpdateOne(context.Background(), filter, update)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode("Failed to update task status")
+			return
+		}
+	}
+
+	response := map[string]string{"uuid": subTask.UUID}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h Handler)  CheckStatus(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	uuid := r.FormValue("uuid")
+
+	collection := h.Client.Database("micro-tasks").Collection("tasks")
+	filter := bson.M{"uuid": uuid}
+
+	var task Task
+	err := collection.FindOne(context.Background(), filter).Decode(&task)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Failed to fetch task")
+		return
+	}
+
+	response := map[string]interface{}{
+		"uuid":   task.UUID,
+		"status": task.Status,
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
--- go.mod
+++ go.mod
@@ -1,13 +1,23 @@
-module micro-manager-tasks/v
+module micro-manager-tasks/m/v2
 
 go 1.21.0
 
 require (
+	github.com/didip/tollbooth/v7 v7.0.1
+	github.com/didip/tollbooth_chi v0.0.0-20220719025231-d662a7f6928f
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/render v1.0.3
 	github.com/gorilla/mux v1.8.1
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/jtrw/go-rest v1.1.0
+	github.com/pkg/errors v0.9.1
 	go.mongodb.org/mongo-driver v1.13.0
 )
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/go-pkgz/expirable-cache v0.1.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
@@ -17,5 +27,6 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
